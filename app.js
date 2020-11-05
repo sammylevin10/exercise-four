3024cf5f815de903ba5dae464e77538acd677c5c
Sammy Levin <sammylevin@macbook-pro-sl.myfiosgateway.com>
2020-11-05 15:38:59 -0500

    Typo fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.POST || 4000;
+const port = process.env.PORT || 4000;
 
 // Routes Import
 const indexRoute = require("./routes/index.js");
