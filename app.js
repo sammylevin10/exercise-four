69fe2b65001b184590410c852dee29558a48625c
Sammy Levin <sammylevin@macbook-pro-sl.myfiosgateway.com>
2020-11-05 14:40:58 -0500

    Added express and environmental files

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-
 const app = express();
-
 const port = process.env.POST || 4000;
 
 // Routes Import
-const indexRoute = require('./routes/index.js');
-// Routes 
-app.use('/', indexRoute);
+const indexRoute = require("./routes/index.js");
+// Routes
+app.use("/", indexRoute);
 
-app.listen(port, () => console.log(`Exercise Four is running at localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Exercise Four is running at localhost:${port}`)
+);
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Exercise Four'))
+router.get("/", (req, res) => res.send("Exercise Four"));
 
 module.exports = router;
