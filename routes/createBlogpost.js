28d5c2e27770cfd3560e34aceae293748867420a
Sammy Levin <sammylevin@macbook-pro-sl.myfiosgateway.com>
2020-11-12 14:51:42 -0500

    Added form to createBlogpost

--- routes/createBlogpost.js
+++ routes/createBlogpost.js
@@ -1,8 +1,22 @@
 // Create blogpost
 const express = require("express");
-
 const router = express.Router();
+// Require firebase
+const firebase = require("firebase");
+// Initialize firestore database
+const db = firebase.firestore();
+// Reference a specific collection
+const blogposts = db.collection("blogposts");
 
-router.get("/", (req, res) => res.send("Create blogpost"));
+const form = `
+<form>
+    <input type = "text" name = "title" placeholder = "Title of post" />
+    <input type = "text" name = "text" placeholder = "Title of post" />
+    <input type = "text" name = "author" placeholder = "Title of post" />
+    <button type = "submit">Submit Post</buttom>
+</form>
+`;
+
+router.get("/", (req, res) => res.send(form));
 
 module.exports = router;
