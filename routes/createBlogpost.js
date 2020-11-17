869cae71363eb9aa4bd9252c75fd2ddccb53b8f1
Sammy Levin <sammylevin@macbook-pro-sl.myfiosgateway.com>
2020-11-17 14:22:36 -0500

    Route for submitting form and using regex to create title ids

--- routes/createBlogpost.js
+++ routes/createBlogpost.js
@@ -9,7 +9,7 @@ const db = firebase.firestore();
 const blogposts = db.collection("blogposts");
 
 const form = `
-<form>
+<form action="/create/submit">
     <input type = "text" name = "title" placeholder = "Title of post" />
     <input type = "text" name = "text" placeholder = "Title of post" />
     <input type = "text" name = "author" placeholder = "Title of post" />
@@ -17,6 +17,25 @@ const form = `
 </form>
 `;
 
+// Default route serves form
 router.get("/", (req, res) => res.send(form));
+
+// Route for submitting the form
+router.get("/submit", (req, res) => {
+  const queryParams = req.query;
+  // Custom IDs for our posts
+  // Using Regex to process our title string into an id
+  const idFromTitle = queryParams.title.replace(/\s+/g, "-").toLowerCase();
+  blogposts
+    .doc(idFromTitle) // Allows you to create new posts or update them
+    .set(queryParams) //?title=text&text=text&author=text
+    .then(function (doc) {
+      res.send("Successful submission");
+    })
+    .catch(function (error) {
+      console.log("error", error);
+      res.send("Failed submission");
+    });
+});
 
 module.exports = router;
